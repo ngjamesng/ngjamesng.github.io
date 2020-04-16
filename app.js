afb8ecba850866891fd543a7ad8c2b322444322e
ngjamesng <just.james.ng@gmail.com>
2020-04-15 21:15:59 -0700

    add secret msg

---
+++ app.js
@@ -0,0 +1,7 @@
+console.log(`
+**************
+hello, if you're looking here, thank you for visiting! 
+I don't expect anyone to read this. 
+I'm looking for the right place to work that will help me, a person who loves teamwork and constant learning! 
+I hope I'm a good fit. 
+**************`)
